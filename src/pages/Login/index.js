409dba33fa2c8e8e81a6ddff040c588d29d90fd5
Muhammad Fikri <fikri2347@gmail.com>
2020-09-27 07:42:27 +0700

    Cek history navigation with history.go()

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,14 @@
 import { createBrowserHistory } from 'history'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {Redirect,useHistory} from 'react-router-dom'
 // import {createBrowserHistory} from 'history'
 const Login = (props) => {
-    const history = createBrowserHistory()
+    const history = useHistory()
     const gotoDashboard = ()=>{
-        window.location.href = "/"
+        history.push('/')
+        history.go(0)
     }
+
     return (
         <>
          <button onClick={gotoDashboard}>Dashboard</button>   
